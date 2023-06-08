217c56df275cfb25764a74f915f9eccc61d2077b
Ubuntu <wtvmandtrill@wtvm.t5mqczan4fhevcyhvriybj0gtf.bx.internal.cloudapp.net>
2023-06-08 16:35:43 +0000

    added token

--- wt_cart/application/__init__.py
+++ wt_cart/application/__init__.py
@@ -1,6 +1,10 @@
 import config
 import configparser
 from apiflask import APIFlask
+from flask_cors import CORS
 
 app = APIFlask(__name__,title="user_api_wt",version='1.0.0',spec_path='/spec')
+cors = CORS(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
 from wt_cart.application.controller.CartController import CartController

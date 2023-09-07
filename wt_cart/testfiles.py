39f8b9950af6eabdeae69450b54e28e1675b0eab
woofandtrill <woofandtrill@gmail.com>
2023-09-06 20:47:02 -0700

    added uuid for address

--- wt_cart/testfiles.py
+++ wt_cart/testfiles.py
@@ -3,14 +3,14 @@ import requests,time
 session = requests.Session()
 session.maxsize = 100
 start_time = time.time()
-url = 'http://localhost:5008/checkout_with_login'
+url = 'http://34.125.89.30:5008/checkout_with_login'
 
-jwt_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoieWNtZGVmbWU5cWc4QlNSTVNQcVYxcEtaZkg5MyIsInNlc3Npb25faWQiOiJnaHNnZGhzaDc4NzM2NzNod2dpa2siLCJleHAiOjE2OTQwMTIwNjZ9.bCeCykW1DzQzcLMPs2xKP9OCYFBOIyF_wSm8IUL6iFA"
+jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoieWNtZGVmbWU5cWc4QlNSTVNQcVYxcEtaZkg5MyIsInNlc3Npb25faWQiOiJnaHNnZGhzaDc4NzM2NzNod2dpa2siLCJleHAiOjE2OTQwNTg0ODJ9.vyN6BWK8Z-6qierHJQTlKx_XZm8eQwUmQ-y5EMOjNE4"
 
-item_id = '201816_03_01'
+item_id = '201819_03_01'
 
 
-response = session.post(url, json={'item_id': item_id,'count':1,'is_active':'1','discount_code':'','address_code': "550e8400-e29b-41d4-a716-446655440000"}, headers={'Authorization': f'Bearer {jwt_token}'})
+response = session.post(url, json={'item_id': item_id,'count':1,'is_active':'1',"discount_code":'',"address_code":'550e8400-e29b-41d4-a716-44665544000'}, headers={'Authorization': f'Bearer {jwt_token}'})
 time= time.time()-start_time
 print(time)
 
